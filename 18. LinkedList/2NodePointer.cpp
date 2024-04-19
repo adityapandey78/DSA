9a102320f04704cb19b08efcd8fbacff78e7386f
adityapandey78 <adityapandey.2402@gmail.com>
2024-04-19 15:28:29 +0530

    .

--- 18. LinkedList/2NodePointer.cpp
+++ 18. LinkedList/2NodePointer.cpp
@@ -9,24 +9,43 @@ public:
         this->next=NULL;
     }
 };
+
+void display(Node*head){
+    Node * temp=head;
+    while(temp!=NULL){
+        cout<<temp->val<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+
+//displaying recursively
+
+void displayRec(Node * head){
+    if(head==NULL) return;
+    displayRec(head->next);
+    cout<<head->val<<" ";
+}
+
+void reverseDisplay(Node* head){
+    if(head==NULL) return; //base case
+    displayRec(head->next); //call
+    cout<<head->val<<" "; //print
+}
 int main(){
     Node* a=new Node(10);
     Node* b=new Node(20);
     Node* c=new Node(30);
     Node* d=new Node(40);
+    Node* e=new Node(50);
 
     a->next=b;
     b->next=c;
     c->next=d;
+    d->next=e;
     //cout<<c->val<<" ";
 
-    //printing the nodes
-    Node*temp=a;
-    while (temp!=NULL)
-    {
-        cout<<temp->val<<" ";
-        temp=temp->next;
-    }
-    
+   display(a); //passing down a as head node
+   reverseDisplay(a);
 return 0;
 }
