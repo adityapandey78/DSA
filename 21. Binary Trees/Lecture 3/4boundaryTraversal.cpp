82bd5111a97c00dadea68fd048479397f88440f6
Aditya Pandey <140270777+adityapandey78@users.noreply.github.com>
2025-02-06 13:03:13 +0530

    .

--- 21. Binary Trees/Lecture 3/4boundaryTraversal.cpp
+++ 21. Binary Trees/Lecture 3/4boundaryTraversal.cpp
@@ -41,23 +41,66 @@ Node* construct(int arr[],int n){
     }
     return root; 
 }
-// void levelOrderTraversal(Node*root){
-//    queue<Node*>q;
-//    q.push(root);
-//     while(q.size()>0){
-//        Node* temp=q.front();
-//        q.pop();
-//        cout<<temp->val<<" ";
-//        if(temp->left!=NULL) q.push(temp->left); 
-//        if(temp->right!=NULL) q.push(temp->right); 
-//     }
-// }
+int levels(Node*root){
+    if(root==NULL) return 0;
+    return 1+max(levels(root->left),levels(root->right));
+}
+
+void NthLevel(Node*root ,int curr, int level){
+    if (root==NULL) return;
+    if(curr==level)cout<<root->val<<" "; //work
+    NthLevel(root->left,curr+1, level); //left
+    NthLevel(root->right,curr+1,level); // right
+}
+void levelOrderTraversal(Node*root){
+    int n=levels(root);
+    for (int i = 0; i <= n; i++)
+    {
+        NthLevel(root,1,i);
+        cout<<endl;
+    }
+    
+}
+
+void leftBoundary(Node*root){
+    if(root==NULL) return;
+    if(root->left==NULL && root->right==NULL)return; //jab leaf node me reach ho toh return
+    cout<<root->val<<" ";
+    leftBoundary(root->left);
+    if(root->left==NULL) leftBoundary(root->right);
+    //jab left me null ho tbhi right me jaao
+}
+void rightBoundary(Node*root){
+    if(root==NULL) return;
+    if(root->left==NULL && root->right==NULL)return; //jab leaf node me reach ho toh return
+    rightBoundary(root->right);
+    if(root->right==NULL) rightBoundary(root->left);
+    cout<<root->val<<" ";
+    //jab left me null ho tbhi right me jaao
+}
+
+void bottomBoundary(Node*root){
+    if(root==NULL) return;
+    if(root->left==NULL && root->right==NULL) //jab leaf node me reach ho toh print kr do
+    cout<<root->val<<" ";
+    bottomBoundary(root->left);
+    bottomBoundary(root->right);
+}
+
 int main(){   
    //construct
-   int arr[]={1,2,3,4,5,6,INT_MIN,INT_MIN,7,8,9};
+int arr[]={1,2,3,4,5,INT_MIN,6,7 ,INT_MIN,INT_MIN,8,INT_MIN,9,10,INT_MIN,11,INT_MIN,12,INT_MIN,13, INT_MIN, 14,15,16,INT_MIN,17,INT_MIN,INT_MIN,18,INT_MIN,19,INT_MIN,INT_MIN,20,21,22,23,INT_MIN,24,25,26,27,INT_MIN,28,INT_MIN};
    int n=sizeof(arr)/sizeof(arr[0]);
    Node* root=construct(arr,n);
 
    levelOrderTraversal(root);
+   cout<<endl<<endl<<endl;
+    cout << root->val << " "; // Print the root node
+    leftBoundary(root->left); // Print the left boundary excluding the leaf nodes
+    bottomBoundary(root->left); // Print the leaf nodes from the left subtree
+    bottomBoundary(root->right); // Print the leaf nodes from the right subtree
+    rightBoundary(root->right); // Print the right boundary excluding the leaf nodes
+    cout << endl;
+
     return 0;
 }
